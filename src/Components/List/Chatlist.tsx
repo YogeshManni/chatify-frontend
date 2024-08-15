4d096fb5f24f99923250a2d7f5feae2491360d49
Yogesh Manni <yogeshmanni786@gmail.com>
2024-08-15 16:29:26 -0700

    chat component done

--- src/Components/List/Chatlist.tsx
+++ src/Components/List/Chatlist.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Avatar, Button, List } from 'antd';
+import { Avatar, Button, Input, List } from 'antd';
 import "./Chatlist.css"
 
 
@@ -20,10 +20,24 @@ function Chatlist() {
           title: 'Tom Hidelton',
         },
       ];
-
+ const filterUsers = (e:any) =>
+ {
+  console.log( e.target.value);
+ }
   return (
-    <div className='flex flex-col w-[30%] border-r-[1px] border-[rgba(255,255,255,0.2)] chatlist p-4 cursor-pointer' >
+    <div className='flex flex-col w-[30%] border-r-[1px] border-[rgba(255,255,255,0.2)] chatlist p-4' >
+       
+       <div className='flex pb-3'>
+          <Avatar size={50} src={`https://api.dicebear.com/7.x/miniavs/svg?seed=0`}/>
+          <h2 className='mt-5'>Yogesh Manni</h2>
+       </div>
+
+       <div className='search pb-5 border-b-[1px] border-[rgba(255,255,255,0.2)]'>
+        <Input.Search  className='bg-transparent text-white-500' placeholder="input search text" allowClear onChange={filterUsers} style={{ width: 200 }} />
+       </div>
+       
         <List
+        className='cursor-pointer'
     itemLayout="horizontal"
     dataSource={data}
     renderItem={(item, index) => (
