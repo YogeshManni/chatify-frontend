4482ea7b0ade06b0729d8052a05368c98ee2aa36
Yogesh Manni <yogeshmanni786@gmail.com>
2024-08-16 16:08:52 -0700

    Add interface to send messages

--- src/Components/Chat/Chat.tsx
+++ src/Components/Chat/Chat.tsx
@@ -1,8 +1,32 @@
 import { InfoCircleFilled,VideoCameraFilled,PhoneFilled,PictureFilled,SendOutlined } from '@ant-design/icons'
 import { Avatar, Button, Input, Space } from 'antd'
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { Interface } from 'readline'
 
 function Chat() {
+
+ interface Message {
+    msg : string, 
+    timestamp: string,
+    isMine:boolean
+ }
+
+
+ const [messages, setMessages] = useState<Message[]>([]);
+ const[msg, setMsg] = useState('');
+ const sendMessage = () =>
+ {
+   console.log(messages)
+    if(msg.length > 0 && msg != "")
+    {
+    const message = {msg:msg, timestamp: new Date().toISOString(),
+        isMine: true,}
+    setMessages([...messages,message])
+    setMsg('')
+    
+    }
+    
+ }
   return (
     <div className='chatContainer flex flex-col lg:w-[50%] w-[100%] border-r-[1px] border-[rgba(255,255,255,0.2)] py-2'>
         <div className='userInfo flex border-b-[1px] border-[rgba(255,255,255,0.2)]'>
@@ -19,16 +43,16 @@ function Chat() {
             </div>
 
         </div>
-        <div className='chatArea flex h-[85%]  border-b-[1px] border-[rgba(255,255,255,0.2)]'>
-
+        <div className='chatArea flex h-[85%]  p-5 border-b-[1px] border-[rgba(255,255,255,0.2)]'>
+        <Avatar size={35} src={`https://api.dicebear.com/7.x/miniavs/svg?seed=6`}/>
         </div>
         <div className='sendMsg flex'>
             <div className='flex p-5'>
                 <PictureFilled style={{ fontSize: '22px'}}/>
             </div>
             <Space.Compact className='w-[100%] p-5'>
-      <Input placeholder="Write a message to send" size="large"/>
-      <Button type="primary" size="large"><SendOutlined /></Button>
+      <Input onChange={(e) => setMsg(e.target.value)} value={msg} placeholder="Write a message to send" size="large"/>
+      <Button type="primary" size="large" onClick={sendMessage}><SendOutlined /></Button>
     </Space.Compact>
        </div>
     </div>
