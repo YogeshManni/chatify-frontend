726fb7b56ecc3b0adeca7d72bdb6a9eb83db406c
Yogesh Manni <yogeshmanni786@gmail.com>
2024-08-30 16:15:48 -0700

    Display chat user name on header of chat component

--- src/Components/Chat/Chat.tsx
+++ src/Components/Chat/Chat.tsx
@@ -127,19 +127,24 @@ function Chat({ chatId }: any) {
       {/*************  User Info  *************/}
 
       <div className="userInfo flex border-b-[1px] border-[rgba(255,255,255,0.2)]">
-        <Avatar
-          size={50}
-          src={`https://api.dicebear.com/7.x/miniavs/svg?seed=0`}
-        />
-        <div className="flex flex-col mt-2 pb-2">
-          <span className="text-[15px]"> Yogesh Manni</span>
-          <span className="text-[13px]">Status is here............ </span>
-        </div>
-        <div className="userOptions flex gap-5 ml-auto pr-5 cursor-pointer">
-          <PhoneFilled style={{ fontSize: "20px" }} />
-          <VideoCameraFilled style={{ fontSize: "20px" }} />
-          <InfoCircleFilled style={{ fontSize: "20px" }} />
-        </div>
+        <>
+          <Avatar
+            size={50}
+            src={`https://api.dicebear.com/7.x/miniavs/svg?seed=0`}
+          />
+          <div className="flex flex-col mt-2 pb-2">
+            <span className="text-[15px]">
+              {" "}
+              {chatId ? chatId.username : "Select a user to chat"}
+            </span>
+            <span className="text-[13px]">Status is here............ </span>
+          </div>
+          <div className="userOptions flex gap-5 ml-auto pr-5 cursor-pointer">
+            <PhoneFilled style={{ fontSize: "20px" }} />
+            <VideoCameraFilled style={{ fontSize: "20px" }} />
+            <InfoCircleFilled style={{ fontSize: "20px" }} />
+          </div>
+        </>
       </div>
 
       {/*************  Chat Area  *************/}
